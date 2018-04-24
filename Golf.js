daf99183d5759c1685aad20735cd31685e0893ce
Ken Gamble <34339890+Keng38@users.noreply.github.com>
2018-04-24 14:43:17 +0100

    Add files via upload

---
+++ Golf.js
@@ -0,0 +1,81 @@
+ //Hole 1 - Youtube.com and click share button. 
+ //Take the end code after https://youtu.be/ and paste into relevant area in each src=' section
+ //between the embed/ and the ?rel text
+
+ //EXAMPLE - src='https://www.youtube.com/embed/C3xu643IrEE?rel=0&amp;showinfo=0&autoplay=1'
+ //PASTE over the C3xu643IrEE between https://www.youtube.com/embed/ and ?rel=0&amp;showinfo=0&autoplay=1'
+
+ //Hole 1//
+    function video(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/x6MadEl2giI?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 2//
+    function video1(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/C3xu643IrEE?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 3//
+     function video2(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/kLK08o1LptU?rel=0&amp;showinfo=0&autoplay=1' encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 4//
+    function video3(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/guUxBlgds-8?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 5//
+    function video4(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/WTQSWD0h1Lo?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 6//
+    function video5(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/Sav8uDMZmok?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 7//
+
+    function video6(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/dS2ELrp2kbI?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 8//
+
+    function video7(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/wrv6YBZihhE?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 9//
+    function video8(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/mkow41_Kn_o?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 10//
+    function video9(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/tfdkUluIeyE?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 11//
+      function video10(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/XcRpAcdAbE8?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 12//
+    function video11(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/KEWnOe6MjTg?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 13//
+    function video12(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/KckKsw4FLrI?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 14//
+    function video13(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/CYOM5aLoq8k?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 15//
+    function video14(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/QApJ1SxJtxY?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 16//
+     function video15(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/5r2ACKiflqc?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 17//
+    function video16(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/MjVsZTr8sOE?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
+//Hole 18//
+    function video17(){
+    document.getElementById("video").innerHTML = "<iframe width='840' height='470' frameborder='0' src='https://www.youtube.com/embed/qN7aCO3R2VE?rel=0&amp;showinfo=0&autoplay=1'  encrypted-media' allowfullscreen></iframe>";
+    }
